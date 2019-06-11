290b9c5be8f7c73a7e0cdc05aeba03a97d733e18
Damith <Damith@DESKTOP-DAVBFGL>
2019-06-11 12:19:53 +0530

    Licence Module Repo Create

---
+++ ExtensionMethods.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LicenceModule
+{
+    public static class ExtensionMethods
+    {
+        public static Tuple<int, int> CoordinatesOf<T>(this T[,] matrix, T value)
+        {
+            int w = matrix.GetLength(0); // width
+            int h = matrix.GetLength(1); // height
+
+            for (int x = 0; x < w; ++x)
+            {
+                for (int y = 0; y < h; ++y)
+                {
+                    if (matrix[x, y].Equals(value))
+                        return Tuple.Create(x, y);
+                }
+            }
+
+            return Tuple.Create(-1, -1);
+        }
+
+        private static char GetOriginalKey(int rand, char val)
+        {
+            char[,] randArr = generateRandomSecret(rand);
+            var newXY = ExtensionMethods.CoordinatesOf(originalArr, val);
+            var originalKey = randArr[newXY.Item1, newXY.Item2];
+            return originalKey;
+        }
+
+        private static char GetAlternativeKey(char[,] arr, char[,] randArr, char key)
+        {
+
+            var originalXY = ExtensionMethods.CoordinatesOf(arr, key);
+            var newKey = randArr[originalXY.Item1, originalXY.Item2];
+            return newKey;
+        }
+
+
+
+        private static int GenerateRandom()
+        {
+            Random random = new Random();
+            return random.Next(1, 4);
+        }
+
+        private static char[,] generateRandomSecret(int rand)
+        {
+            char[,] array2D = new char[4, 7];
+
+
+            char[] array = getAlphabetArray();
+            int[] indexOrder = getIndexOrder(rand);
+
+            int setidx = 0;
+            int idx = 0;
+
+            bool done = false;
+
+            while (!done)
+            {
+                foreach (var item in indexOrder)
+                {
+                    if (item == idx)
+                    {
+                        int i = item;
+                        for (int j = 0; j < array2D.GetLength(1); j++)
+                        {
+                            array2D[Array.IndexOf(indexOrder, item), j] = array[setidx++];
+                        }
+                        idx++;
+                        if (item == 3)
+                        {
+                            done = true;
+                        }
+                    }
+
+                }
+            }
+
+
+            return array2D;
+        }
+
+        private static int[] getIndexOrder(int rand)
+        {
+            int[] indexOrder = new int[4];
+            int x = rand - 1;
+            for (int i = 0; i < 4; i++)
+            {
+                if (x < 4)
+                {
+                    indexOrder[x++] = i;
+                }
+                else
+                {
+                    indexOrder[x++ - 4] = i;
+                }
+            }
+
+            Console.Write("Index Order Secret :");
+            foreach (var item in indexOrder)
+            {
+                Console.Write(item.ToString());
+            }
+            Console.WriteLine();
+
+            return indexOrder;
+        }
+
+        private static char[] getAlphabetArray()
+        {
+            char[] array = new char[28];
+
+            int index = 0;
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                array[index] = c;
+                index++;
+            }
+
+            for (int x = 0; index < array.Length; index++)
+            {
+                array[index] = Convert.ToChar(x.ToString());
+                x++;
+            }
+            return array;
+        }
+
+        private static char[,] getGeneralSecret()
+        {
+            char[,] array2D = new char[4, 7];
+
+            char[] array = getAlphabetArray();
+
+            int setidx = 0;
+            for (int i = 0; i < array2D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2D.GetLength(1); j++)
+                {
+                    array2D[i, j] = array[setidx++];
+                }
+            }
+            return array2D;
+        }
+        private static char[,] getGeneralOppositeSecret()
+        {
+            char[,] array2D = new char[4, 7];
+
+            char[] array = getAlphabetArray();
+
+            int setidx = 0;
+            for (int i = 0; i < array2D.GetLength(1); i++)
+            {
+                for (int j = 0; j < array2D.GetLength(0); j++)
+                {
+                    array2D[j, i] = array[setidx++];
+                }
+            }
+
+
+
+            return array2D;
+        }
+
+        private static char[,] getCustomSecret()
+        {
+            char[,] array2D = new char[4, 7] {
+            { 'Y' , 'D' , 'H' , 'T' , 'P' , 'B' , 'I' },
+            { 'M' , '0' , 'O' , 'G' , 'E' , 'R' , '1' },
+            { 'X' , 'U' , 'A' , 'S' , 'W' , 'K' , 'J' },
+            { 'Q' , 'F' , 'L' , 'N' , 'C' , 'Z' , 'V' } };
+            return array2D;
+        }
+        public static char[,] originalArr { get; set; }
+
+        public static bool ValidateClient(int rand, char original, char random)
+        {
+            char[,] Arr = originalArr = getCustomSecret();
+            char[,] randArr = generateRandomSecret(rand);
+            return (original == GetOriginalKey(rand, random));
+            
+        }
+    }
+}
---
+++ LicenceCheck.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace LicenceModule
+{
+    public static class LicenceCheck
+    {
+
+        public static LicenceValidity CheckValidLicence(string key, string serverDate, string client)
+        {
+            var licenceValidity = new LicenceValidity { IsCheckProcessSuccess = false };
+
+            string ParseLevel = "Initial";
+
+            try
+            {
+                if (key == null || key == "")
+                {
+                    throw new Exception("Licence Key Not Applied");
+                }
+
+                if(serverDate == null || serverDate == "")
+                {
+                    throw new Exception("Server date Error");
+                }
+
+                if(client == null || client == "")
+                {
+                    throw new Exception("Client code Error");
+                }
+
+
+                var splitServerDate = serverDate.Split('-');
+                var serverDateActual = new DateTime(Convert.ToInt16(splitServerDate[2]), Convert.ToInt16(splitServerDate[1]),
+                    Convert.ToInt16(splitServerDate[0]), 23, 59, 59);
+
+                ParseLevel = "Server Date";
+
+                var keyWithNoDash = key.Replace("-", string.Empty);
+                var keyArray = keyWithNoDash.ToCharArray();
+
+                ParseLevel = "Key Decode Stage 1";
+                var asciiSum = keyArray[8] + keyArray[20];
+                var asciiSumString = asciiSum.ToString();
+                var checkSum1 = asciiSumString.Substring(0, 1);
+                var checkSum2 = asciiSumString.Substring(asciiSumString.Length - 1, 1);
+                ParseLevel = "Key Decode Stage 1";
+                var fullAsciiSum = keyArray[1] + keyArray[3] + keyArray[4] + keyArray[6] + keyArray[7] + keyArray[8] +
+                                   keyArray[10] + keyArray[12] + keyArray[13] + keyArray[15] + keyArray[16] + keyArray[18] +
+                                   keyArray[19] + keyArray[20] + keyArray[22] + keyArray[25] + keyArray[28];
+                var fullAsciiSumString = fullAsciiSum.ToString();
+                var fullCkeckSum1 = fullAsciiSumString.Substring(0, 1);
+                var fullCkeckSum2 = fullAsciiSumString.Substring(fullAsciiSumString.Length - 1, 1);
+                ParseLevel = "Key Decode Stage 1";
+                var clientShortArray = client.ToCharArray();
+                var clientShortAsciiSum = clientShortArray[0] + clientShortArray[1];
+                var clientShortAsciiSumString = clientShortAsciiSum.ToString();
+                var clientShortAsciiSumLast = clientShortAsciiSumString.Substring(clientShortAsciiSumString.Length - 1, 1);
+                ParseLevel = "Key Decode Stage 2";
+                var clientShortAsciiSumLastInt = Convert.ToInt16(clientShortAsciiSumLast);
+                var clientShortChecksum = Math.Abs(clientShortAsciiSumLastInt - Convert.ToInt16(keyArray[6].ToString()));
+
+                var clientIdxOne = clientShortArray[0];
+
+                ParseLevel = "Key Decode Stage 3";
+
+                var clientLongCheckSum = clientShortAsciiSum * Convert.ToInt16(string.Concat(keyArray[26].ToString(), keyArray[27].ToString()));
+                var clientLongCheckSumString = clientLongCheckSum.ToString();
+                var clientLongCheckSum1 = clientLongCheckSumString.Substring(clientLongCheckSumString.Length - 2, 1);
+                var clientLongCheckSum2 = clientLongCheckSumString.Substring(clientLongCheckSumString.Length - 1, 1);
+
+                ParseLevel = "Key Decode Stage 4";
+
+                var a = Convert.ToInt16(fullCkeckSum2) == Convert.ToInt16(keyArray[26].ToString());
+                var b = Convert.ToInt16(fullCkeckSum1) == Convert.ToInt16(keyArray[27].ToString());
+                var c = Convert.ToInt16(checkSum2) == Convert.ToInt16(keyArray[2].ToString());
+                var d = Convert.ToInt16(checkSum1) == Convert.ToInt16(keyArray[14].ToString());
+                var e = clientShortChecksum == Convert.ToInt16(keyArray[18].ToString());
+                var f = Convert.ToInt16(clientLongCheckSum1) == Convert.ToInt16(keyArray[21].ToString());
+                var g = Convert.ToInt16(clientLongCheckSum2) == Convert.ToInt16(keyArray[9].ToString());
+                var h = ExtensionMethods.ValidateClient(Convert.ToInt32(Char.GetNumericValue(keyArray[6])), clientShortArray[0], keyArray[0]);
+
+                ParseLevel = "Key Decode Stage 5";
+
+                if (Convert.ToInt16(fullCkeckSum2) == Convert.ToInt16(keyArray[26].ToString()) &&
+                    Convert.ToInt16(fullCkeckSum1) == Convert.ToInt16(keyArray[27].ToString()) &&
+                    Convert.ToInt16(checkSum2) == Convert.ToInt16(keyArray[2].ToString()) &&
+                    Convert.ToInt16(checkSum1) == Convert.ToInt16(keyArray[14].ToString()) &&
+                    clientShortChecksum == Convert.ToInt16(keyArray[18].ToString()) &&
+                    Convert.ToInt16(clientLongCheckSum1) == Convert.ToInt16(keyArray[21].ToString()) &&
+                    Convert.ToInt16(clientLongCheckSum2) == Convert.ToInt16(keyArray[9].ToString()) )
+                {
+                    licenceValidity.IsValid = true;
+                }
+                else
+                {
+                    licenceValidity.IsValid = false;
+                    licenceValidity.IsCheckProcessSuccess = true;
+                    return licenceValidity;
+                }
+
+                ParseLevel = "Key Decode Stage 6";
+
+                var expireKey = 480402 * asciiSum;
+                var expireKeyString = expireKey.ToString();
+                var expireKeySub = expireKeyString.Substring(expireKeyString.Length - 5, 5);
+                ParseLevel = "Key Decode Stage 7";
+                var expiryKeyYear =
+                    string.Concat((Convert.ToInt16(keyArray[29].ToString()) - Convert.ToInt16(keyArray[6].ToString())).ToString(),
+                        Convert.ToInt16(keyArray[24].ToString()).ToString());
+                var expiryKeyMonth = string.Concat(Convert.ToInt16(keyArray[11].ToString()).ToString(),
+                    Convert.ToInt16(keyArray[5].ToString()).ToString());
+                var expiryKeyDay = string.Concat(Convert.ToInt16(keyArray[23].ToString()).ToString(),
+                    Convert.ToInt16(keyArray[17].ToString()).ToString());
+                ParseLevel = "Key Decode Stage 8";
+                var expiryValue = Convert.ToInt64(String.Concat(expiryKeyYear, expiryKeyDay, expiryKeyMonth));
+                var expiryDateString = expiryValue - Convert.ToInt64(expireKeySub);
+
+                if (expiryDateString <= 0)
+                {
+                    licenceValidity.IsValid = false;
+                    licenceValidity.IsCheckProcessSuccess = true;
+                    return licenceValidity;
+                }
+                ParseLevel = "Key Decode Stage 9";
+                var expiryYear = string.Concat("20", expiryDateString.ToString().Substring(0, 2));
+                var expiryMonth = expiryDateString.ToString().Substring(4, 2);
+                var expiryDay = expiryDateString.ToString().Substring(2, 2);
+                ParseLevel = "Key Decode Stage 10";
+                var expiryDate = licenceValidity.ExpiryDate = new DateTime(Convert.ToInt16(expiryYear), Convert.ToInt16(expiryMonth), Convert.ToInt16(expiryDay), 23, 59, 59);
+
+                licenceValidity.IsExpired = (expiryDate - serverDateActual).TotalDays < 0;
+                licenceValidity.IsCheckProcessSuccess = true;
+                return licenceValidity;
+            }
+            catch (Exception e)
+            {
+                licenceValidity.CheckProcessFailedReason = ParseLevel +" @ "+ e.Message;
+                return licenceValidity;
+            }
+        }
+
+        public struct LicenceValidity
+        {
+            public bool IsCheckProcessSuccess;
+            public string CheckProcessFailedReason;
+            public bool IsValid;
+            public bool IsExpired;
+            public DateTime ExpiryDate;
+        }
+    }
+}
